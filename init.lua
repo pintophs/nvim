51042318915785a9c899eda7ea08df87260623ae
pintophs <94130906+pintophs@users.noreply.github.com>
2025-09-06 18:55:19 -0300

    Add VimTeX

--- init.lua
+++ init.lua
@@ -145,5 +145,16 @@ require('lazy').setup({
       indent = { enable = true, disable = { 'ruby' } },
     },
   },
+
+  -- VimTeX
+  {
+  "lervag/vimtex",
+  lazy = false,     -- we don't want to lazy load VimTeX
+  -- tag = "v2.15", -- uncomment to pin to a specific release
+  init = function()
+    -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_view_method = "zathura"
+  end
+  },
 })
 
